7b141d116c1fecf3ed2f0b1db4e6a403bcbd0e2b
马扬杰 <ma-yangjie@outlook.com>
2021-03-27 16:25:41 +0800

    Add project files.

---
+++ Ch05Ex02/Ch05Ex02/Program.cs
@@ -0,0 +1,42 @@
+﻿using static System.Console;
+using System;
+
+namespace Ch05Ex02
+{
+    enum orientation: byte
+    {
+        north = 1,
+        south = 2,
+        east = 3,
+        west = 4
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte DirectionByte;
+            string DirectionString;
+            orientation myDirection = orientation.north;
+            WriteLine($"myDirection = {myDirection}");
+            DirectionByte = (byte)myDirection;
+            DirectionString = myDirection.ToString();
+            WriteLine($"byte equivalent = {DirectionByte}");
+            WriteLine($"string equivalent = {DirectionString}");
+
+            // loop enum values
+            foreach(var i in Enum.GetValues(typeof(orientation)))
+            {
+                Console.WriteLine(i);
+            }
+
+            foreach (ConsoleColor color in Enum.GetValues(typeof(ConsoleColor)))
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine($"Foreground Color set to {color}");
+            }
+
+            Console.WriteLine(typeof(System.ComponentModel.DisplayNameAttribute));
+        }
+    }
+}
---
+++ Ch05TypeConversion/Ch05TypeConversion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ch05TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 281 = 100011001
+            // 25 =  000011001
+            // 255 = 011111111
+            byte destinationVar;
+            short sourceVar = 281;
+            destinationVar = (byte)sourceVar;
+            Console.WriteLine($"sourceVar val: {sourceVar}");
+            Console.WriteLine($"destinationVar val: {destinationVar}");
+        }
+    }
+}
