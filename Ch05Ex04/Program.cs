ec65e761fad950dba78ae7fbbb577317e87b018a
马扬杰 <ma-yangjie@outlook.com>
2021-03-27 17:14:08 +0800

    update

---
+++ Ch05Ex04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace Ch05Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendNames = { "Todd Anthony", "Kevin Holton", "Shane Laigle" };
+            WriteLine($"Here are {friendNames.Length} of my friends:");
+            
+            //for (int i = 0; i < friendNames.Length; i++)
+            //{
+            //    WriteLine(friendNames[i]);
+            //}
+
+            foreach(string friendName in friendNames)
+            {
+                WriteLine(friendName);
+            }
+        }
+    }
+}
---
+++ Ch05Ex05/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+namespace Ch05Ex05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendNames = { "Todd Anthony", "Kevin Holton", 
+                                    "Shane Laigle", null, "" };
+
+            foreach(string friendName in friendNames)
+            {
+                switch (friendName)
+                {
+                    case string t when t.StartsWith("T"):
+                        WriteLine($"This friends name starts with a 'T': " +
+                                    $"{friendName} and is {t.Length - 1} letters long.");
+                        break;
+                    case string e when e.Length == 0:
+                        WriteLine("There is a string in the array with no value");
+                        break;
+                    case null:
+                        WriteLine("There was a 'null' value in the array");
+                        break;
+                    case var x:
+                        WriteLine("This is the var pattern of type: " +
+                                    $"{x.GetType().Name}");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
