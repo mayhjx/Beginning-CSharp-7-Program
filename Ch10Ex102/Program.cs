77e0263b904d83aec660e8084104de322756be36
mayangjie <mayhjx@foxmail.com>
2021-04-04 00:17:58 +0800

    finished ch10

---
+++ Ch10Ex102/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Ch10Ex101;
+
+namespace Ch10Ex102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class MyDerivedClass : MyClass
+    {
+        public override string GetString() =>
+            base.GetString() + " (output from derived class)"; ;
+    }
+}
---
+++ Ch10Ex105/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Ch10CardLib;
+
+namespace Ch10Ex105
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck myDeck = new Deck();
+            myDeck.Shuffle();
+
+            for (int i =  0; i < 47; i += 5)
+            {
+                Card[] cards = new Card[5];
+                for (int time = 0; time < 5; time++)
+                {
+                    Console.WriteLine($"{i+time} {myDeck.GetCard(i+time)}");
+                    cards[time] = myDeck.GetCard(i + time);
+                }
+                
+                if(cards[0].suit == cards[1].suit &&
+                    cards[1].suit == cards[2].suit &&
+                    cards[2].suit == cards[3].suit &&
+                    cards[3].suit == cards[4].suit)
+                {
+                    Console.Write($"{cards[0].ToString()} " +
+                        $"{cards[1].ToString()} " +
+                        $"{cards[2].ToString()} " +
+                        $"{cards[3].ToString()} " +
+                        $"{cards[4].ToString()} Flush!");
+                    return;
+                }
+
+            }
+
+            Console.WriteLine("No flush.");
+        }
+    }
+}
