ad7f30699a84b9535d52860a064313bdaef81e7a
马扬杰 <ma-yangjie@outlook.com>
2021-04-09 16:50:10 +0800

    finished ch11

---
+++ Ch11Conversions/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+namespace Ch11Conversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvClass1 op1 = new ConvClass1();
+            op1.val = 3;
+            ConvClass2 op2 = op1;
+            Console.WriteLine(op2.val);
+
+            ConvClass2 op3 = new ConvClass2();
+            op3.val = 3e5;
+            ConvClass1 op4 = (ConvClass1)op3;
+            Console.WriteLine(op4.val);
+        }
+    }
+
+    public class ConvClass1
+    {
+        public int val;
+        public static implicit operator ConvClass2(ConvClass1 op1)
+        {
+            ConvClass2 returnVal = new ConvClass2();
+            returnVal.val = op1.val;
+            return returnVal;
+        }
+    }
+
+    public class ConvClass2
+    {
+        public double val;
+        public static explicit operator ConvClass1(ConvClass2 op1)
+        {
+            ConvClass1 returnVal = new ConvClass1();
+            checked { returnVal.val = (int)op1.val; };
+            return returnVal;
+        }
+    }
+
+}
---
+++ Ch11Ex05/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using static System.Console;
+
+namespace Ch11Ex05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add(new Person("Rual", 30));
+            list.Add(new Person("Donna", 25));
+            list.Add(new Person("Mary", 27));
+            list.Add(new Person("Ben", 44));
+
+            WriteLine("Unsorted people:");
+            for (int i = 0; i < list.Count; i++)
+            {
+                WriteLine($"{(list[i] as Person).Name} ({(list[i] as Person).Age})");
+            }
+            WriteLine();
+
+            WriteLine("People sorted with default comparer (by age):");
+            list.Sort();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                WriteLine($"{(list[i] as Person).Name} ({(list[i] as Person).Age})");
+            }
+            WriteLine();
+
+            WriteLine("People sorted with nondefault comparer (by name):");
+            list.Sort(PersonComparerName.Default);
+            for (int i = 0; i < list.Count; i++)
+            {
+                WriteLine($"{(list[i] as Person).Name} ({(list[i] as Person).Age})");
+            }
+
+        }
+    }
+}
