c02c1e832363b3fd1dc0c4786c2a7906e8746943
马扬杰 <ma-yangjie@outlook.com>
2021-04-12 18:01:25 +0800

    event

---
+++ Ch13Ex01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Timers;
+using static System.Console;
+
+namespace Ch13Ex01
+{
+    class Program
+    {
+        static int counter = 0;
+        static string displayString = 
+            "This string will appear one letter at a time. ";
+
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer(200);
+            myTimer.Elapsed += new ElapsedEventHandler(WriteChar);
+            myTimer.Start();
+
+            // give the timer the opportunity to start sending message to the console application.
+            System.Threading.Thread.Sleep(200); 
+
+            ReadKey(); // important
+        }
+
+        static void WriteChar(object source, ElapsedEventArgs e)
+        {
+            Write(displayString[counter++ % displayString.Length]);
+        }
+    }
+}
---
+++ Ch13Ex02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Timers;
+using static System.Console;
+
+namespace Ch13Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connection myConnection = new Connection();
+            Display myDisplay = new Display();
+            myConnection.MessageArrived +=
+                new MessageHandler(myDisplay.DisplayMessage);
+            myConnection.Connect();
+            ReadKey();
+        }
+    }
+}
