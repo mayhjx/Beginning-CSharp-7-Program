000778f2e84c11a7d740e0984002aba7ed668fb3
马扬杰 <ma-yangjie@outlook.com>
2021-04-12 18:01:13 +0800

    custom exception

---
+++ Ch13CardClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Ch13CardLib;
+
+namespace Ch13CardClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck1 = new Deck();
+            try
+            {
+                Card myCard = deck1.GetCard(60);
+            }
+            catch (CardOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.DeckContents[0]);
+            }
+        }
+    }
+}
