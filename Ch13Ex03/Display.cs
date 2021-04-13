abfe5cfcc5a48ab6afce82f56f148c91cb624cf1
马扬杰 <ma-yangjie@outlook.com>
2021-04-13 11:52:39 +0800

    update

---
+++ Ch13Ex03/Display.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch13Ex03
+{
+    class Display
+    {
+        public void DisplayMessage(object source, MessageArrivedEventArgs e)
+        {
+            WriteLine($"Message arrived from: {(source as Connection)?.Name}");
+            WriteLine($"Message arrived: {e.Message}");
+        }
+    }
+}
