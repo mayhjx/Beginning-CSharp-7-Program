ace25a75dfe07b649c968c359958953a680dea34
马扬杰 <ma-yangjie@outlook.com>
2021-04-20 18:02:52 +0800

    exercise 13.1

---
+++ Ch13Ex131/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Timers;
+
+namespace Ch13Ex131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Timer timer = new Timer();
+            
+        }
+    }
+}
