3d40312d0f94d516dfbb31b4edd8882d186b1d4d
马扬杰 <ma-yangjie@outlook.com>
2021-04-20 17:41:32 +0800

    using lambda expression with collections

---
+++ Ch13Ex09/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+namespace Ch13Ex09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Donna", "Mary", "Andrea" };
+
+            WriteLine(people.Aggregate(
+                (a, b) => a + " " + b));
+
+            WriteLine(people.Aggregate<string, int>(
+                0,
+                (a, b) => a + b.Length));
+
+            WriteLine(people.Aggregate<string, string, string>(
+                "Some people:",
+                (a, b) => a + " " + b,
+                a => a));
+
+            WriteLine(people.Aggregate<string, string, int>(
+                "Some people:",
+                (a, b) => a + " " + b,
+                a => a.Length));
+
+        }
+    }
+}
